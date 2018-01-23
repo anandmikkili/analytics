b380d23b2d7e25c1e30c839828f2d38a4bef5b16
anandmikkili <anandmikkili@gmail.com>
2018-01-23 16:09:52 +0530

    Add files via upload

--- ui.R
+++ ui.R
@@ -23,7 +23,7 @@ shinyUI(
       )),
     dashboardBody(
       tabItems(
-        tabItem("trainPS", fluidPage(
+        tabItem("trainPS", h2("PREDICTION SUMMARY"),fluidPage(
           fluidRow(
             tabsetPanel(
               tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainps")),
@@ -31,7 +31,7 @@ shinyUI(
               tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainps"))
             )
           ))),
-        tabItem("trainOv", fluidPage(
+        tabItem("trainOv", h2("OVERALL PREDICTION"),fluidPage(
           fluidRow(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainov")),
@@ -39,7 +39,7 @@ shinyUI(
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainov"))
           )
         ))),
-        tabItem("trainOH", fluidPage(
+        tabItem("trainOH", h2("HIGH VALUE CUSTOMERS"),fluidPage(
           fluidRow(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainOH")),
@@ -47,84 +47,84 @@ shinyUI(
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainOH"))
           )
         ))),
-      tabItem("trainOM", fluidPage(
+      tabItem("trainOM", h2("MEDIUM VALUE CUSTOMERS"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainOM")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotlyOutput("plot_trainOM", height = "200px"),selectInput("select", "Select", label = h2("Selection Pane"),choices = list(PredictedNonUser = "PredictedNonUser", PredictedUser = "PredictedUser",ActualChurners="ActualChurners",GrandTotal="GrandTotal"))),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainOM"))
           )
         )),
-      tabItem("trainOL", fluidPage(
+      tabItem("trainOL", h2("LOW VALUE CUSTOMERS"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainOL")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotlyOutput("plot_trainOL", height = "200px"),selectInput("select", "Select", label = h2("Selection Pane"),choices = list(PredictedNonUser = "PredictedNonUser", PredictedUser = "PredictedUser",ActualChurners="ActualChurners",GrandTotal="GrandTotal"))),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainOL"))
           )
         )),
-      tabItem("traincpiScore", fluidPage(
+      tabItem("traincpiScore", h2("CHURN PROBABILITY INDEX"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainCPI")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotlyOutput("plot_trainCPI", height = "200px"),selectInput("select", "Select", label = h2("Selection Pane"),choices = list(ModelPredictedNonUsercount="ModelPredictedNonUsercount",ActualChurners="ActualChurners",ModelPostValidationAccuracy="ModelPostValidationAccuracy"))),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainCPI"))
           )
         )),
-      tabItem("trainaon", fluidPage(
+      tabItem("trainaon", h2("AGE ON NETWORK"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainAON")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_trainAON")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainAON"))
           )
         )),
-      tabItem("traintariff", fluidPage(
+      tabItem("traintariff", h2("TARIFF DETAILS"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainTrf")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_trainTrf")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainTrf"))
           )
         )),
-      tabItem("trainregion", fluidPage(
+      tabItem("trainregion", h2("DEMOGRAPHIC DETAILS"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainRgn")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_trainRgn")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainRgn"))
           )
         )),
-      tabItem("trainvaluesegment", fluidPage(
+      tabItem("trainvaluesegment", h2("VALUE SEGMENTATION"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_trainValue")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_trainValue")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_trainValue"))
           )
         )),
-      tabItem("testpredstatus", fluidPage(
+      tabItem("testpredstatus", h2("PREDICTION STATUS"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_testStatus")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_testStatus")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_testStatus"))
           )
         )),
-      tabItem("testcpiScore", fluidPage(
+      tabItem("testcpiScore", h2("CHURN PROBABILITY INDEX"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_testCPI")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_testCPI")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_testCPI"))
           )
         )),
-      tabItem("testaon", fluidPage(
+      tabItem("testaon", h2("AGE ON NETWORK"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_testAON")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_testAON")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_testAON"))
           )
         )),
-      tabItem("testtariff", fluidPage(
+      tabItem("testtariff", h2("TARIFF DETAILS"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_testTrf")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_testTrf")),
             tabPanel(title="Text",icon = icon("th"),verbatimTextOutput("text_testTrf"))
           )
         )),
-      tabItem("testregion", fluidPage(
+      tabItem("testregion", h2("DEMOGRAPHIC DETAILS"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"), tableOutput("table_testRgn")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_testRgn")),
@@ -132,7 +132,7 @@ shinyUI(
           )
         )),
         
-      tabItem("testvaluesegment", fluidPage(
+      tabItem("testvaluesegment", h2("VALUE SEGMENTATION"),fluidPage(
           tabsetPanel(
             tabPanel(title="Table",icon = icon("table"),tableOutput("table_testValue")),
             tabPanel(title="Plot",icon = icon("bar-chart-o"),plotOutput("plot_testValue")),
